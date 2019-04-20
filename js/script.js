bacfe9088cfecaae6d11d392d753754b985169db
shafali03 <shaf03sm@gmail.com>
2019-04-20 06:22:43 +0100

    update

---
+++ js/script.js
@@ -0,0 +1,118 @@
+
+// immediately invoke function
+(function () {
+
+
+    const services = [
+        {
+            value: 1,
+            title: 'great - 20%'
+        },
+        {
+            value: 1,
+            title: 'OK - 10%'
+        },
+        {
+            value: 1,
+            title: 'bad - 2%'
+        },
+    ]
+
+
+    // Loop through the array Select option
+    services.forEach(function (service) {
+
+        const option = document.createElement('option');
+        option.textContent = service.title;
+        option.value = service.value;
+
+        document.getElementById('input-service').appendChild(option);
+
+
+    });
+
+
+    // get all the value
+
+    const form = document.getElementById('tip-form');
+    const amount = document.getElementById('input-bill');
+    const users = document.getElementById('input-users');
+    const service = document.getElementById('input-service');
+
+    // customer feedback
+    const feedback = document.querySelector('.feedback');
+    const loader = document.querySelector('.loader');
+    const results = document.querySelector('.results');
+
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        let bill = amount.value;
+        // console.log(bill);
+        let people = users.value;
+        let quality = service.value;
+
+
+        if (
+            bill === '' ||
+            bill <= '0' ||
+            (people === '' || people <= '0') ||
+            quality === '0') {
+
+            feedback.classList.add('showItem', 'alert-danger');
+            feedback.innerHTML = `
+            <p>please check the values</p>
+            <p>bill amount can not be less than zero</p>
+            <p>people sharing the bill cannot be less than zero</p>
+            <p>service has to be selected</p>
+            `;
+            setTimeout(function () {
+                feedback.classList.remove('showItem', 'alert-danger');
+            }, 10000);
+        } else {
+            feedback.classList.add('showItem', 'alert-success');
+            feedback.innerHTML = `<p>calculating...</p>`;
+            loader.classList.add('showItem');
+            setTimeout(function () {
+                loader.classList.remove('showItem');
+                feedback.classList.remove('showItem', 'alert-success');
+
+
+                showResults(bill, people, quality);
+                clearForm();
+            }, 4000)
+        }
+    });
+    // show results
+    function showResults(bill, people, quality) {
+        let percent = 0;
+
+        if (quality === '1') {
+            percent = 0.2;
+        }
+        else if (quality === '2') {
+            percent = 0.1;
+        }
+        else if (quality === '3') {
+            percent = 0.02
+        }
+
+        let tipAmount = parseInt(bill) * percent;
+        let total = parseInt(bill) + tipAmount;
+        let person = total / parseInt(people);
+
+        results.classList.add('showItem');
+        document.getElementById('tip-amount').textContent = tipAmount;
+        document.getElementById('total-amount').textContent = total;
+        document.getElementById('person-amount').textContent = person.toFixed(2);
+    }
+
+
+    // clear form
+    function clearForm() {
+        amount.value = "";
+        users.value = "";
+        service.value = "";
+    }
+
+})();
